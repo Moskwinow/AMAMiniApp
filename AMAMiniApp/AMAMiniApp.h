6a2dd11daaddda61f1ba15848303e98f7d2f021e
Maksym <maksym.vechirko@axiomc.com>
2021-10-14 12:49:06 +0300

    Initial Commit

---
+++ AMAMiniApp/AMAMiniApp.h
@@ -0,0 +1,18 @@
+//
+//  AMAMiniApp.h
+//  AMAMiniApp
+//
+//  Created by Maksym on 14/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMAMiniApp.
+FOUNDATION_EXPORT double AMAMiniAppVersionNumber;
+
+//! Project version string for AMAMiniApp.
+FOUNDATION_EXPORT const unsigned char AMAMiniAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMAMiniApp/PublicHeader.h>
+
+
